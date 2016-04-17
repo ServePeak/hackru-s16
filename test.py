9e6b6cee5feadc0065b25d09399ec1f6ac769cef
Yunbin Zhang <mzhangyb@gmail.com>
2016-04-17 00:49:22 -0400

    updating new options

--- test.py
+++ test.py
@@ -7,10 +7,22 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def get_wiki():
     """Respond and greet the caller by name."""
-
     sent_message = request.values.get('Body', None)
-    wiki = wikipedia.page(sent_message)
-    message = wiki.url
+    args = sent_message.split(" ",1);
+    if (args[0].lower() == "search"):
+        message = wikipedia.search(args[1])
+    else if (args[0].lower() == "content"):
+        page = wikipedia.page(args[1])
+		message = page.url
+    else if (args[0].lower() == "url"):
+        page = wikipedia.page(args[1])
+		message = page.url
+    else:
+        #default sends back summary of the search
+		message = wikipedia.summary(sent_message)
+	
+	#message to send back
+    #message = wiki.url
     
     resp = twilio.twiml.Response()
     resp.message(message)
