ba701a37122c1ae6cadc49ff7394d7b39023b989
ServePeak <ratedrion@gmail.com>
2016-04-16 22:21:03 -0400

    twilio test

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-import os
-from flask import Flask
-
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello world!"
-
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+ 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
