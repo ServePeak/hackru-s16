3f93a70f370967f61090ee553e63976388cb967d
ServePeak <ratedrion@gmail.com>
2016-04-16 23:50:31 -0400

    wikipedia url from message

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import wikipedia
  
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
+def get_wiki():
+    """Respond and greet the caller by name."""
+
+    sent_message = request.values.get('Body', None)
+    wiki = wikipedia.page(sent_message)
+    message = wiki.url
+    
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(message)
+ 
     return str(resp)
  
 if __name__ == "__main__":
